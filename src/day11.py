43b18ecb6e4a46ad4fcaf6897bf466327e08ae47
Dan Foster <dan@zem.org.uk>
2023-12-11 13:05:03 +0000

    day11 p2

--- src/day11.py
+++ src/day11.py
@@ -10,19 +10,19 @@ class Point:
     x: int
     y: int
 
-    def expand_universe_x(self, cols: Set[int]) -> None:
+    def expand_universe_x(self, cols: Set[int], multiplier: int) -> None:
         count: int = 0
         for col in cols:
             if col < self.x:
                 count += 1
-        self.x += count
+        self.x += count * multiplier
 
-    def expand_universe_y(self, rows: Set[int]) -> None:
+    def expand_universe_y(self, rows: Set[int], multiplier: int) -> None:
         count: int = 0
         for row in rows:
             if row < self.y:
                 count += 1
-        self.y += count
+        self.y += count * multiplier
 
     def calc_distance(self, other: "Point") -> int:
         return abs(self.x - other.x) + abs(self.y - other.y)
@@ -30,17 +30,18 @@ class Point:
 
 class Day11:
     def __init__(self, input_filename: str = "day11.txt") -> None:
-        self.parse_data(self.read_file(input_filename))
+        self.raw_data = self.read_file(input_filename).rstrip().split("\n")
+        self.parse_data()
 
     @staticmethod
     def read_file(filename: str) -> str:
         with open(os.path.join("inputs", filename), "r") as file:
             return file.read()
 
-    def parse_data(self, data: str) -> None:
+    def parse_data(self) -> None:
         self.data: List[Point] = []
         _id: int = 1
-        for y, row in enumerate(data.rstrip().split("\n")):
+        for y, row in enumerate(self.raw_data):
             for x, c in enumerate(row):
                 if c == "#":
                     self.data.append(Point(_id, x, y))
@@ -48,7 +49,7 @@ class Day11:
         self.width = x + 1
         self.height = y + 1
 
-    def expand_universe(self) -> None:
+    def expand_universe(self, multiplier: int = 1) -> None:
         cols1 = set(range(self.width))
         cols2 = set([x.x for x in self.data])
         cols = cols1 - cols2
@@ -56,27 +57,33 @@ class Day11:
         rows2 = set([x.y for x in self.data])
         rows = rows1 - rows2
         for point in self.data:
-            point.expand_universe_x(cols)
-            point.expand_universe_y(rows)
+            point.expand_universe_x(cols, multiplier=multiplier)
+            point.expand_universe_y(rows, multiplier=multiplier)
         pass
 
     def get_pairs(self) -> Iterable[tuple[Point, Point]]:
         return combinations(self.data, 2)
 
-    def part1(self) -> int:
+    def calc_ans(self, multiplier: int = 1) -> int:
         ans: int = 0
-        self.expand_universe()
+        if multiplier > 1:
+            # WHY?!
+            multiplier -= 1
+        self.expand_universe(multiplier=multiplier)
         pairs = self.get_pairs()
         for pair in pairs:
             ans += pair[0].calc_distance(pair[1])
         return ans
 
-    def part2(self) -> int:
-        ans: int = 0
-        return ans
+    def part1(self) -> int:
+        return self.calc_ans()
+
+    def part2(self, multiplier: int = 1000000) -> int:
+        return self.calc_ans(multiplier)
 
 
 if __name__ == "__main__":
     day = Day11()
     print(day.part1())
+    day.parse_data()
     print(day.part2())
--- src/test_day11.py
+++ src/test_day11.py
@@ -8,9 +8,12 @@ class TestDay11:
 
     def test_part2_example(self) -> None:
         day = Day("day11_e1.txt")
-        assert day.part2() == 1
+        assert day.part2(multiplier=10) == 1030
+        day.parse_data()
+        assert day.part2(multiplier=100) == 8410
 
     def test(self) -> None:
         day = Day("day11.txt")
-        assert day.part1() == 1
-        assert day.part2() == 1
+        assert day.part1() == 9795148
+        day.parse_data()
+        assert day.part2() == 650672493820
